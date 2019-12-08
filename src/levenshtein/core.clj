4839090074929e87498cbc3dfe67818cd02d7a35
Slava Istomin <visla.vvi@gmail.com>
2019-12-08 05:13:38 +0500

    the first version.

---
+++ src/levenshtein/core.clj
@@ -0,0 +1,45 @@
+(ns levenshtein.core
+  (:gen-class)
+  (:require
+    [clojure.string :as s]))
+
+; // len_s and len_t are the number of characters in string s and t respectively
+; int LevenshteinDistance(const char *s, int len_s, const char *t, int len_t)
+; { 
+;   int cost;
+
+;   /* test if last characters of the strings match */
+;   if (s[len_s-1] == t[len_t-1])
+;       cost = 0;
+;   else
+;       cost = 1;
+
+;   /* return minimum of delete char from s, delete char from t, and delete char from both */
+;   return minimum(LevenshteinDistance(s, len_s - 1, t, len_t    ) + 1,
+;                  LevenshteinDistance(s, len_s    , t, len_t - 1) + 1,
+;                  LevenshteinDistance(s, len_s - 1, t, len_t - 1) + cost);
+; }
+
+(defn without-last-char
+  "Returns the given word without the last char"
+  [x]
+  (cond
+    (s/blank? x) x
+    :else (subs x 0 (dec (count x)))))
+
+(defn last-equal?
+  "Are the last chars of two words are equal?"
+  [a b]
+  (= (take-last 1 a) (take-last 1 b)))
+
+(defn f
+  [a b]
+  (cond
+    (s/blank? a) (count b)
+    (s/blank? b) (count a)
+    :else (min (inc (f (without-last-char a) b))
+               (inc (f a (without-last-char b)))
+               (+ (f (without-last-char a) (without-last-char b))
+                  (if (last-equal? a b) 0 1)))))
+
+(def levenshtein-distance f)
---
+++ test/levenshtein/core_test.clj
@@ -0,0 +1,24 @@
+(ns levenshtein.core-test
+  (:require [clojure.test :refer :all]
+            [levenshtein.core :refer :all]))
+
+(deftest without-last-char-test
+  (is (= (without-last-char "") ""))
+  (is (= (without-last-char "a") ""))
+  (is (= (without-last-char "ab") "a")))
+
+(deftest last-equal?-test
+  (is (true? (last-equal? "a" "a")))
+  (is (true? (last-equal? "ab" "ab")))
+  (is (false? (last-equal? "ba" "ab"))))
+
+(deftest distance-between-blank-and-something
+  (testing "blank and something"
+    (is (= (levenshtein-distance "" "") 0))
+    (is (= (levenshtein-distance "x" "") 1))
+    (is (= (levenshtein-distance "" "x") 1)))
+  (testing "the rest of the cases"
+    (is (= (levenshtein-distance "abc" "ab") 1))
+    (is (= (levenshtein-distance "ab" "abc") 1))
+    (is (= (levenshtein-distance "ab" "bc") 2))
+    (is (= (levenshtein-distance "water" "w1t1r") 2))))
