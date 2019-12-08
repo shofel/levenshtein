b54ff55a06f7463f16da1833f7dd00e063c80569
Slava Istomin <visla.vvi@gmail.com>
2019-12-08 06:00:48 +0500

    remove the fragment copied from wikipedia.

--- src/levenshtein/core.clj
+++ src/levenshtein/core.clj
@@ -4,23 +4,6 @@
     [clojure.math.combinatorics :as combo]
     [clojure.string :as str]))
 
-; // len_s and len_t are the number of characters in string s and t respectively
-; int LevenshteinDistance(const char *s, int len_s, const char *t, int len_t)
-; { 
-;   int cost;
-
-;   /* test if last characters of the strings match */
-;   if (s[len_s-1] == t[len_t-1])
-;       cost = 0;
-;   else
-;       cost = 1;
-
-;   /* return minimum of delete char from s, delete char from t, and delete char from both */
-;   return minimum(LevenshteinDistance(s, len_s - 1, t, len_t    ) + 1,
-;                  LevenshteinDistance(s, len_s    , t, len_t - 1) + 1,
-;                  LevenshteinDistance(s, len_s - 1, t, len_t - 1) + cost);
-; }
-
 (defn without-last-char
   "Returns the given word without the last char"
   [x]
